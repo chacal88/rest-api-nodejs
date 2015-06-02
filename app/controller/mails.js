c561e04306b04e34d3012f884b73a6a5001528ac
chacal88 <kauemsc@gmail.com>
2015-06-02 17:03:58 -0300

    documentacao do serviço

--- app/controller/mails.js
+++ app/controller/mails.js
@@ -1,9 +1,16 @@
 var mails = {
+	/**
+	 * Servico para envio de email Simples 
+	 * Paramns: 
+	 * to: string - Email do destinatario - Obrigatorio
+	 * name: string - Nome do destinatario
+	 * subject: string - Assunto do email - Obrigatorio
+	 * body: string - Corpo do email - Obrigatorio
+	 */
 	schedule : function(req, res) {
-
 		var validate = true;
 		validate = validateMail(req);
-		if ( !validate ) {
+		if (!validate) {
 			var Agenda = require('../components/Agenda');
 			var agenda = new Agenda();
 			agenda.schedule(req);
@@ -11,28 +18,30 @@ var mails = {
 				'message' : "Sucesso email enviado",
 				"req" : req.body
 			});
-		}else{
-			res.status(403).send({'message': validate,"req":req.body});
+		} else {
+			res.status(403).send({
+				'message' : validate,
+				"req" : req.body
+			});
 		}
 	}
 };
+/**
+ * Validação de envio
+ */
 function validateMail(req) {
 	var to = req.body.to || '';
 	var body = req.body.body || '';
 	var subject = req.body.subject || '';
 	if (to == '') {
 		return "Parametro 'to' invalido. " + to;
-
 	}
 	if (subject == '') {
 		return "Parametro 'subject' invalido. " + subject;
-
 	}
 	if (body == '') {
 		return "Parametro 'body' invalido. " + body;
-
 	}
 	return false;
 }
-
 module.exports = mails;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,29 +1,27 @@
-module.exports = function routes( router , app) {
-	
+module.exports = function routes( router , app) {	
 	app.all( app.get('domain') + '/'+ app.get('version') + '/private/*', [ require( appRoot + '/lib/middleware/validateRequest' ) ]);
 	
 	var appConst = require( app.get('constants') + '/app');
 	var mails = require(app.get('controller') + '/mails.js');
 	
-	/*
+	/**
 	 * Routes that can be accessed by any one
 	 */
 	//router.post( '/' + appConst.DOMAIN + '/' + appConst.VERSION + '/public/login', auth.login);
 	router.post( '/' + appConst.DOMAIN + '/' + appConst.VERSION + '/public/mails' , mails.schedule );
 	
 
-//	 
-//	/*
-//	 * Routes that can be accessed only by autheticated users
-//	 */
+	/**
+	 * Routes that can be accessed only by autheticated users
+	 */
 //	router.get( appConst.DOMAIN + '/' + appConst.VERSION + '/private' + '/products' , products.getAll );
 //	router.get( appConst.DOMAIN + '/' + appConst.VERSION + '/private' + '/:id' , products.getOne );
 //	router.post( appConst.DOMAIN + '/' + appConst.VERSION + '/private' + '/' , products.create );
 //	router.put ( appConst.DOMAIN + '/' + appConst.VERSION + '/private' + '/:id' , products.update );
 //	router.delete( appConst.DOMAIN + '/ '+ appConst.VERSION + '/private' + '/:id' , products.delete );
-//	/*
-//	 * Routes that can be accessed only by authenticated & authorized users
-//	 */
+	/**
+	 * Routes that can be accessed only by authenticated & authorized users
+	 */
 //	router.get( appConst.DOMAIN + '/'+ appConst.VERSION + '/private' + '/admin/users', user.getAll );
 //	router.get( appConst.DOMAIN + '/'+ appConst.VERSION + '/private' + '/user/:id', user.getOne );
 //	router.post('/' + appConst.DOMAIN + '/'+ appConst.VERSION + '/public' + '/user', user.create );
